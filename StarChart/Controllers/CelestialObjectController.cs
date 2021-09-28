44a67b01c9cf56e6976a0ca709c01b33d5336cd3
tijokjoy <tijokjoy@gmail.com>
2021-09-28 11:52:34 +0100

    Set up the controller class

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController : Controller
+    {
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        RouteAttribute Route = new RouteAttribute("");
+        ApiControllerAttribute ApiController = new ApiControllerAttribute();
+
+        private readonly ApplicationDbContext _context;
+
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
